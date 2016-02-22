0f6e46184c29ac2ebe49c384242780a19f50160c
Akash <acwankhede@gmail.com>
2016-02-22 17:10:40 +0530

    added admin.py file by altering models

--- irctc/irctc/admin.py
+++ irctc/irctc/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 admin.autodiscover()
-from irctc.models import UserDetail,Train,Reservation,Passenger,Station,Location
+from irctc.models import UserDetail,Train,Reservation,Station,Location
 # Register your models here.
 
 admin.site.register(UserDetail)
 admin.site.register(Train)
 admin.site.register(Reservation)
-admin.site.register(Passenger)
 admin.site.register(Station)
 admin.site.register(Location)
